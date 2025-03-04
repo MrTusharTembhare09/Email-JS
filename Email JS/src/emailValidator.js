98dae6ad02cb820449a3dd49bd1e7c0ef9852d5f
MrTusharTembhare09 <tushartembhare147@gmail.com>
2025-03-04 19:26:17 +0530

    [Tushar] Add. Handled optional 2-character country code TLD along with tests

--- Email JS/src/emailValidator.js
+++ Email JS/src/emailValidator.js
@@ -1,11 +1,12 @@
-// Validating mandatory and optional parts of the email
+// Validating mandatory and optional parts of the email including optional 2-character TLD
 function validateEmail(email) {
-    const emailRegex = /^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@bridgelabz\.co(\..+)?$/;
+    const emailRegex = /^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-zA-Z]{2})?$/;
     return emailRegex.test(email);
 }
 
 // Exporting function for testing
 module.exports = validateEmail;
+
 
 
 
--- Email JS/test/emailValidator.test.js
+++ Email JS/test/emailValidator.test.js
@@ -8,16 +8,16 @@ test('Valid Email: abc.xyz@bridgelabz.co', () => {
     expect(validateEmail('abc.xyz@bridgelabz.co')).toBe(true);
 });
 
-test('Valid Email: abc_xyz@bridgelabz.co', () => {
-    expect(validateEmail('abc_xyz@bridgelabz.co')).toBe(true);
+test('Valid Email: abc_xyz@bridgelabz.co.in', () => {
+    expect(validateEmail('abc_xyz@bridgelabz.co.in')).toBe(true);
 });
 
-test('Valid Email: abc-xyz@bridgelabz.co', () => {
-    expect(validateEmail('abc-xyz@bridgelabz.co')).toBe(true);
+test('Valid Email: abc-xyz@bridgelabz.co.us', () => {
+    expect(validateEmail('abc-xyz@bridgelabz.co.us')).toBe(true);
 });
 
-test('Valid Email: abc+xyz@bridgelabz.co.in', () => {
-    expect(validateEmail('abc+xyz@bridgelabz.co.in')).toBe(true);
+test('Valid Email: abc+xyz@bridgelabz.co.uk', () => {
+    expect(validateEmail('abc+xyz@bridgelabz.co.uk')).toBe(true);
 });
 
 test('Invalid Email: abc@bridgelabzcom', () => {
@@ -28,12 +28,10 @@ test('Invalid Email: abc@xyz.co', () => {
     expect(validateEmail('abc@xyz.co')).toBe(false);
 });
 
-test('Invalid Email: abc**xyz@bridgelabz.co', () => {
-    expect(validateEmail('abc**xyz@bridgelabz.co')).toBe(false);
+test('Invalid Email: abc@bridgelabz.co.ind', () => {
+    expect(validateEmail('abc@bridgelabz.co.ind')).toBe(false);
 });
 
-test('Invalid Email: abc#xyz@bridgelabz.co', () => {
-    expect(validateEmail('abc#xyz@bridgelabz.co')).toBe(false);
+test('Invalid Email: abc@bridgelabz.co.123', () => {
+    expect(validateEmail('abc@bridgelabz.co.123')).toBe(false);
 });
-
-
