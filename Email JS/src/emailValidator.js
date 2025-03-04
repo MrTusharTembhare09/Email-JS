7b569e064bb303725a5f15a7b9d2ed8e67be695d
MrTusharTembhare09 <tushartembhare147@gmail.com>
2025-03-04 19:16:32 +0530

    [Tushar] Add. Ensured '.' after 'bridgelabz' and validated mandatory 'co' along with tests

--- Email JS/src/emailValidator.js
+++ Email JS/src/emailValidator.js
@@ -1,9 +1,10 @@
-// Validating mandatory parts of email: '@' and 'bridgelabz'
+// Validating mandatory parts of email: '@', 'bridgelabz', '.', and 'co'
 function validateEmail(email) {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@bridgelabz\..+$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@bridgelabz\.co(\..+)?$/;
     return emailRegex.test(email);
 }
 
 // Exporting function for testing
 module.exports = validateEmail;
+
 
--- Email JS/test/emailValidator.test.js
+++ Email JS/test/emailValidator.test.js
@@ -1,22 +1,22 @@
 const validateEmail = require('../src/emailValidator');
 
-test('Valid Email: abc@bridgelabz.com', () => {
-    expect(validateEmail('abc@bridgelabz.com')).toBe(true);
+test('Valid Email: abc@bridgelabz.co', () => {
+    expect(validateEmail('abc@bridgelabz.co')).toBe(true);
 });
 
-test('Valid Email: xyz@bridgelabz.in', () => {
-    expect(validateEmail('xyz@bridgelabz.in')).toBe(true);
+test('Valid Email: xyz@bridgelabz.co.in', () => {
+    expect(validateEmail('xyz@bridgelabz.co.in')).toBe(true);
 });
 
-test('Invalid Email: abc@xyz.com', () => {
-    expect(validateEmail('abc@xyz.com')).toBe(false);
+test('Invalid Email: abc@bridgelabzcom', () => {
+    expect(validateEmail('abc@bridgelabzcom')).toBe(false);
 });
 
-test('Invalid Email: abc.bridgelabz.com', () => {
-    expect(validateEmail('abc.bridgelabz.com')).toBe(false);
+test('Invalid Email: abc@bridgelabz.xyz', () => {
+    expect(validateEmail('abc@bridgelabz.xyz')).toBe(false);
 });
 
-test('Invalid Email: abc@bridgelabz', () => {
-    expect(validateEmail('abc@bridgelabz')).toBe(false);
+test('Invalid Email: abc@xyz.co', () => {
+    expect(validateEmail('abc@xyz.co')).toBe(false);
 });
 
